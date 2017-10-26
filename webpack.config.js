2d52c62276f911d246a14fad4a346f77af0c0ce6
Antonio Quintero Felizzola <antonio@Antonios-MacBook-Pro.local>
2017-10-26 18:18:28 -0400

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,62 @@
-// entry -> output
+// Entry -> output
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+// To extract the styles into their own file:
+// 1. Require the new plugin
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = (env) => {
+
+    const isProduction = env === 'production';
+
+    // 2. Make a new instace of ExtractTextPlugin. We need to pass a single argument to
+    // the constructor function, this is the name of the file
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                // This is where wwe define exactly how we want the abstraction to work and in our case we do have
+                // some plugins we want to run
+                use: CSSExtract.extract({
+                    use: [
+                        // In order to enable the source map for our css:
+                        //'css-loader',
+                        //'sass-loader'
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        // 3. Pass CSSExtract into the plugins array
+        plugins: [
+            CSSExtract
+        ],
+        //devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
