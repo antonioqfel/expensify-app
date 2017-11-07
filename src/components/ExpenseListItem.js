f84552c420fc93f064df069042337c976883e088
Antonio Quintero Felizzola <antonio@Antonios-MacBook-Pro.local>
2017-11-07 17:46:30 -0500

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,6 +2,8 @@
 // description, amount, createdAt
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
                         // I can continue using the destructered object,  I just need to add dispatch
                         // which come from props
@@ -10,7 +12,12 @@ const ExpenseListItem = ({ amount, createdAt, description, id }) => (
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {/* We divided our number by 100 to convert it from cents */}
+            {numeral(amount / 100).format('$0,0.00')}
+            -
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 );
 
